b18d0986efe934eae542b3c559ca778fdc68cece
Prash <personal4prash@gmail.com>
2025-12-19 23:40:31 +0545

    windows edit3

---
+++ JournalApp/Platforms/Windows/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml;
+
+namespace JournalApp.WinUI;
+
+public class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        Application.Start((p) =>
+        {
+            var context = new Microsoft.UI.Dispatching.DispatcherQueueSynchronizationContext(
+                Microsoft.UI.Dispatching.DispatcherQueue.GetForCurrentThread());
+            System.Threading.SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+    }
+}
