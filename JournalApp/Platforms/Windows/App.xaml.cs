1c48eb23992e3dac3b078eec8028df3575612198
Prash <personal4prash@gmail.com>
2025-12-20 00:26:50 +0545

    windows build17

--- JournalApp/Platforms/Windows/App.xaml.cs
+++ JournalApp/Platforms/Windows/App.xaml.cs
@@ -6,7 +6,6 @@ public partial class App : MauiWinUIApplication
 {
     public App()
     {
-        this.InitializeComponent();
     }
 
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
